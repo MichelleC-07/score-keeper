0f591b5ef0311003375cd5afe04d99e75fedfc2b
MichelleC-07 <chewpoim1308@uwec.edu>
2021-09-04 09:41:52 -0500

    Added colors for scores

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const p1button = document.querySelector('#p1Button')
-const p2button = document.querySelector('#p2Button')
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
+const p1button = document.querySelector('#p1Button');
+const p2button = document.querySelector('#p2Button');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const resetBtn = document.querySelector('#reset');
 
-let p1Score = 0
-let p2Score = 0
-let winningScore = 5
+const winningScoreSelect = document.querySelector('#playTo');
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 3;
 let isGameOver = false;
 
 
@@ -15,6 +18,8 @@ p1button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
     }
     p1Display.textContent = p1Score;
@@ -28,7 +33,28 @@ p2button.addEventListener('click', function () {
     }
     if (p2Score === winningScore) {
         isGameOver = true;
+        p2Display.classList.add('winner');
+        p1Display.classList.add('loser');
     }
     p2Display.textContent = p2Score;
 
 })
+
+winningScoreSelect.addEventListener('change', function () {
+    // alert("SOMETHING HAS CHANGED");
+    // console.log(typeof (this.value)); //this gives a string
+    winningScore = parseInt(this.value);
+    reset(); // <--- execute here
+})
+
+resetBtn.addEventListener('click', reset) // <--- passing a reference 
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
