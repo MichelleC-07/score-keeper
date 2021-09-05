1c4f88370b4e300805d800857f503d7e2edfec92
MichelleC-07 <chewpoim1308@uwec.edu>
2021-09-05 09:51:35 -0500

    added css style

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ p1button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1button.disabled = true;
+            p2button.disabled = true;
         }
     }
     p1Display.textContent = p1Score;
@@ -33,8 +35,10 @@ p2button.addEventListener('click', function () {
     }
     if (p2Score === winningScore) {
         isGameOver = true;
-        p2Display.classList.add('winner');
-        p1Display.classList.add('loser');
+        p2Display.classList.add('has-text-success');
+        p1Display.classList.add('has-text-danger');
+        p2button.disabled = true;
+        p1button.disabled = true;
     }
     p2Display.textContent = p2Score;
 
@@ -55,6 +59,8 @@ function reset() {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1button.disabled = false;
+    p2button.disabled = false;
 }
